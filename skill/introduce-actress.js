4460cdf4c35767a27bf2478b9214af2638544418
塚本 恭輔 <tsukamotok@istyle.local>
2019-08-16 15:44:45 +0900

    Improve

--- skill/introduce-actress.js
+++ skill/introduce-actress.js
@@ -43,12 +43,4 @@ module.exports = class SkillHandleDeliveryOrder {
             },
         }
     }
-
-    async finish(bot, event, context){
-        await bot.reply({
-            type: "text",
-            text: `あいよっ。じゃあ${context.confirmed.menu}を30分後くらいに${context.confirmed.address}にお届けしますわ。おおきに。`
-        });
-    }
-
 }
